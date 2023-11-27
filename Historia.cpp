f9a846909b78d9f63c07514ddc97563c605d2dd1
David Guevara Paniagua <guevara.david1e@gmail.com>
2023-11-27 13:38:41 -0600

    Aniado el menu principal

---
+++ Historia.cpp
@@ -0,0 +1,24 @@
+//Proyecto Final de Programacion
+
+#include<iostream>
+using namespace std;
+
+int main(){
+	char secuenciaDeDecisiones[7];
+	
+	//Menu Principal
+	cout<<"Eres un detective que tiene que resolver un caso. Aun eres un amateur. De este caso depende tu carrera."<<endl;
+	cout<<"En la zona 6 se escucharon gritos de terror a media noche."<<endl:
+	cout<<"Llegas a la casa de donde provienieron las cacofonias"<<endl;
+	do{
+		cout<<"Que haces?"<<endl;
+		cout<<"a) Inspeccionar el suelo."<<endl;
+		cout<<"b) Inspeccionar el estudio"<<endl;
+		cout<<"e) Salir"<<endl;
+		cin>>secuenciaDeDecisiones[0];
+	}while(false); //opcionValida
+	
+	
+	
+	return 0;
+}
