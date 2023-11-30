4a10d92f6584682781b7efa5bdc179e5ae707dcd
David Guevara Paniagua <guevara.david1e@gmail.com>
2023-11-29 18:39:16 -0600

    Aniado el segundo menu

--- Historia.cpp
+++ Historia.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 int main(){
 	char secuenciaDeDecisiones[7];
-	
+	bool opcionValida=false, salir=false;
 	//Menu Principal
 	cout<<"Eres un detective que tiene que resolver un caso. Aun eres un amateur. De este caso depende tu carrera."<<endl;
 	cout<<"En la zona 6 se escucharon gritos de terror a media noche."<<endl:
@@ -30,7 +30,30 @@ int main(){
 				cout<<"Elige una opcion valida"<<endl;
 				opcionValida=false;
 		}
-	}while(false);
+	}while(!opcionValida);
+	
+	//Segundo menu
+	do{
+		cout<<"Que haces ahora?"<<endl;
+		cout<<"a) Tomar una muestra"<<endl;
+		cout<<"b) Inspeccionar el patio trasero"<<endl;
+		cout<<"e) Salir"<<endl;
+		switch (secuenciaDeDecisiones[1]){
+			case 'a':
+				cout<<"Tomas una muestra de la sangre en el piso y la guardas"<<endl;
+			break;
+			case 'b':
+				cout<<"Encuentras un jardin bien cuidado, pero nada que destacar"<<endl;
+			break;
+			case 'e':
+				salir=true;
+			break;
+			default:
+				cout<<"Elige una opcion valida"<<endl;
+				opcionValida=false;
+		}
+	}while(!salir && !opcionValida);
+	
 	
 	
 	
